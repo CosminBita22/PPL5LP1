db0450ca8b7b3348107c08a65f8983969e8e7a74
ezio4322 <bitacosmin22@yahoo.com>
2025-03-21 20:31:11 +0200

    second uhh burr

--- main.py
+++ main.py
@@ -5,8 +5,30 @@ import re
 def get_input():
     user_text = entry.get()
     number_list = re.findall(r'\d+', user_text)
-    label_output.config(text=f"Result: {number_list}")
     return number_list
+
+
+def print_result(text):
+    label_output.config(state="normal")
+    label_output.delete()
+    label_output.insert(tk.END, text)
+    label_output.see(tk.END)
+
+
+def add_list_button():
+    print_result(get_input())
+
+
+def filter_odd(number_list):
+    filtered_number_list = []
+    for i in number_list:
+        if int(i) % 2 != 0:
+            filtered_number_list.append(i)
+    return filtered_number_list
+
+
+def filter_odd_button():
+    print_result(filter_odd(get_input()))
 
 
 root = tk.Tk()
@@ -15,13 +37,16 @@ root.title("Tkinter thingy ")
 frame = tk.Frame(root)
 frame.pack(pady=10)
 
-entry = tk.Entry(frame, width=25)
+entry = tk.Entry(frame, width=100)
 entry.grid(row=0, column=0, padx=5)
 
-button = tk.Button(frame, text="Add list", command=get_input)
+button = tk.Button(frame, text="Add list", command=add_list_button)
 button.grid(row=0, column=1, padx=5)
 
-label_output = tk.Label(root, text="")
+button = tk.Button(frame, text="Filter odd", command=filter_odd_button)
+button.grid(row=1, column=1, padx=5)
+
+label_output = tk.Text(root)
 label_output.pack(pady=10)
 
 root.mainloop()
